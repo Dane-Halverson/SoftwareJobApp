3ddb6139bcdb3062b6b1d859b63a6f34eceedd66
Dane Halverson <halve564@d.umn.edu>
2023-04-27 21:05:40 -0500

    created class for storing city data

---
+++ lib/CityData.dart
@@ -0,0 +1,20 @@
+
+
+class CityData {
+
+  final String city;
+  final double meanSalaryAdjusted;
+  final double meanSalaryUnadjusted;
+  final double meanSalaryAllOccupations;
+  final int numberOfSoftwareJobs;
+  final double costOfLiving;
+  final double rent;
+  final double purchasingPower;
+
+  CityData({required this.city, required this.meanSalaryAdjusted, required this.meanSalaryUnadjusted,
+      required this.meanSalaryAllOccupations, required this.numberOfSoftwareJobs,
+      required this.costOfLiving, required this.rent, required this.purchasingPower,
+  });
+
+
+}
