51a0f9d6c1b53e12f1ce4fb3414ad19294bf6c34
Dane Halverson <halve564@d.umn.edu>
2023-04-30 23:09:46 -0500

    refactored videos to get data from firestore

--- lib/ui/VideosView.dart
+++ lib/ui/VideosView.dart
@@ -1,7 +1,7 @@
 
-import 'package:final_project/videos/models/VideosList.dart';
+import 'package:final_project/videos/VideosList.dart';
 import 'package:flutter/material.dart';
-import 'package:final_project/videos/models/VideosModel.dart';
+import 'package:final_project/videos/VideosModel.dart';
 import 'package:flutter_animated_icons/flutter_animated_icons.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 import 'package:lottie/lottie.dart';
@@ -11,6 +11,8 @@ import 'package:flutter_animated_icons/lottiefiles.dart';
 
 
 class VideosView extends StatelessWidget {
+  const VideosView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -33,17 +35,19 @@ class _VideosStatefulWidgetState extends State<VideosStatefulWidget> with Ticker
   final VideosModel model = VideosModel();
 
   final List<Widget> _pages = <Widget>[];
+  final Future<List<VideosList>> _videos = VideosModel.getVideosList();
 
 
-  late final List<VideosList> _options = model.videos;
+  late List _options;
 
-  int? _value;
+  int _value = 0;
+
+  List<VideosList> videos = [];
 
   @override
   void initState() {
     super.initState();
     _favoriteController = AnimationController(vsync: this, duration: const Duration(seconds: 1));
-    _value = model.videos[0].videos.isNotEmpty ? 0 : 1;
   }
 
   @override
@@ -54,131 +58,155 @@ class _VideosStatefulWidgetState extends State<VideosStatefulWidget> with Ticker
 
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _videos,
+        builder: (BuildContext context, AsyncSnapshot<List<VideosList>> snapshot) {
+          if (snapshot.hasData) {
+            List<VideosList> videosList = snapshot.data!;
+            _options = videosList;
 
-    YoutubePlayerController controller = YoutubePlayerController(
-    initialVideoId: model.videos[0].videos.isNotEmpty ? model.videos.first.videos.first.id :
-    model.videos[1].videos.first.id,
-    flags: const YoutubePlayerFlags(
-    autoPlay: false,
-    mute: false,
-    ));
+            if(videosList.isEmpty) {
+              return const Center(
+                child: Text('no videos'),
+              );
+            }
 
-    _pages.clear();
-    for (int j =0; j < model.videos[0].videos.length; ++j) {
-      if (!model.videos[0].videos[j].isFavorite) {
-        model.videos[0].videos.removeAt(j);
-      }
-    }
 
-    for (int i = 0; i < model.videos.length; ++i) {
-      _pages.add(
-          ListView(
-            children: List<Widget>.generate(
-                model.videos[i].videos.length, (int index) {
-              if (model.videos[i].videos[index].isFavorite) {
-                _favoriteController.reset();
-                _favoriteController.animateTo(0.6);
+            YoutubePlayerController controller = YoutubePlayerController(
+                initialVideoId: videosList[0].videos.isNotEmpty ? videosList
+                    .first.videos.first.id :
+                videosList[1].videos.first.id,
+                flags: const YoutubePlayerFlags(
+                  autoPlay: false,
+                  mute: false,
+                ));
+
+            _pages.clear();
+            for (int j = 0; j < videosList[0].videos.length; ++j) {
+              if (!videosList[0].videos[j].isFavorite) {
+                videosList[0].videos.removeAt(j);
               }
-              return model.videos[i].videos.isNotEmpty? GestureDetector(
+            }
 
-                  onTap: () {
-                    controller.load(model.videos[i].videos[index].id);
-                  },
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Card(
-                            shape: RoundedRectangleBorder(
-                              side: const BorderSide(),
-                              borderRadius:
-                              BorderRadius.circular(20.0), //<-- SEE HERE
-                            ),
-                            child: Row(
-                              children: [
-                                const Icon(
-                                  Icons.play_arrow_outlined,
-                                  size: 40,
-                                ),
-                                Expanded(
-                                  child: Text(
-                                    model.videos[i].videos[index].title,
-                                    style: const TextStyle(
-                                      //color: Colors.white,
-                                      fontFamily: "WorkSans",
-                                      fontSize: 25,
+            for (int i = 0; i < videosList.length; ++i) {
+              _pages.add(
+                  ListView(
+                    children: List<Widget>.generate(
+                        videosList[i].videos.length, (int index) {
+                      if (videosList[i].videos[index].isFavorite) {
+                        _favoriteController.reset();
+                        _favoriteController.animateTo(0.6);
+                      }
+                      return videosList[i].videos.isNotEmpty ? GestureDetector(
+
+                          onTap: () {
+                            controller.load(videosList[i].videos[index].id);
+                          },
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Card(
+                                    shape: RoundedRectangleBorder(
+                                      side: const BorderSide(),
+                                      borderRadius:
+                                      BorderRadius.circular(
+                                          20.0), //<-- SEE HERE
                                     ),
-                                  ),
+                                    child: Row(
+                                      children: [
+                                        const Icon(
+                                          Icons.play_arrow_outlined,
+                                          size: 40,
+                                        ),
+                                        Expanded(
+                                          child: Text(
+                                            videosList[i].videos[index].title,
+                                            style: const TextStyle(
+                                              //color: Colors.white,
+                                              fontSize: 25,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    )),
+                              ),
+                              Padding(padding: const EdgeInsets.only(
+                                  right: 8, left: 0),
+                                child: IconButton(
+                                  onPressed: () {
+                                    if (!videosList[i].videos[index]
+                                        .isFavorite) {
+                                      _favoriteController.reset();
+                                      _favoriteController.animateTo(0.6);
+                                      setState(() {
+                                        videosList[i].videos[index].isFavorite =
+                                        !videosList[i].videos[index].isFavorite;
+                                        if (videosList[i].videos[index]
+                                            .isFavorite) {
+
+                                          VideosModel.addFavorite
+                                            (videosList[i].videos[index].id,
+                                              videosList[i].videos[index].title);
+                                        }
+                                      });
+                                    } else {
+                                      _favoriteController.reverse();
+                                      videosList[i].videos[index].isFavorite =
+                                      !videosList[i].videos[index].isFavorite;
+                                      VideosModel.rmFavorite(videosList[i].videos[index].id);
+                                    }
+                                  },
+                                  icon: Lottie.asset(Icons8.heart_color,
+                                      controller: _favoriteController),
+
+
                                 ),
-                              ],
-                            )),
-                      ),
-                      Padding(padding: const EdgeInsets.only(right: 8, left: 0),
-                      child:  IconButton(
-                        onPressed: () {
-                          if (!model.videos[i].videos[index].isFavorite) {
-                            _favoriteController.reset();
-                            _favoriteController.animateTo(0.6);
-                            setState(() {
-                              model.videos[i].videos[index].isFavorite = !model
-                                  .videos[i].videos[index].isFavorite;
-                              if(model.videos[i].videos[index].isFavorite) {
-                                model.videos[0].videos.add(
-                                    model.videos[i].videos[index]
-                                );
-                              }
-                            });
-                          } else {
-                            _favoriteController.reverse();
-                            model.videos[i].videos[index].isFavorite = !model
-                                .videos[i].videos[index].isFavorite;
-                            if(model.videos[i].videos[index].isFavorite) {
-                              model.videos[0].videos.add(
-                                  model.videos[i].videos[index]
-                              );
-                            }
-                          }
+                              )
+
+                            ],
+                          )) : const Text('Nothing to show');
+                    }),
+                  )
+
+              );
+            }
+
+            return Scaffold(
+                body: Column(children: [
+                  YoutubePlayer(
+                    controller: controller,
+                  ),
+                  SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: List<Widget>.generate(
+                            _options.length, (int index) {
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 4),
+                            child: ChoiceChip(
+                              label: Text(_options[index].type),
+                              selected: _value == index,
+                              onSelected: (bool selected) {
+                                setState(() {
+                                  _value = index;
+                                });
+                              },
+                            ),
+                          );
+                        }),
+                      )),
+                  Expanded(child: _pages.elementAt(_value!) ?? Text("nothing to show")
+                  ),
+                ]));
+          }
+          else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        }
 
 
-                        },
-                        icon: Lottie.asset(Icons8.heart_color, controller: _favoriteController),
+    );
 
-
-              ),
-                      )
-
-                    ],
-                  )) : const Text("Nothing to show");
-            }),
-          )
-      );
-    }
-
-    return Scaffold(
-        body: Column(children: [
-          YoutubePlayer(
-            controller: controller,
-          ),
-          SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: List<Widget>.generate(_options.length, (int index) {
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 4),
-                    child: ChoiceChip(
-                      label: Text(_options[index].type),
-                      selected: _value == index,
-                      onSelected: (bool selected) {
-                        setState(() {
-                          _value = index;
-                        });
-                      },
-                    ),
-                  );
-                }),
-              )),
-          Expanded(child: _pages.elementAt(_value!)),
-        ]));
   }
-
 
 }
