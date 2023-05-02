de1bc2fb23a80272b1ccda020cd32ef3d70399c5
Dane Halverson <halve564@d.umn.edu>
2023-05-02 06:30:35 -0500

    fixed and formatted create account page

--- lib/ui/CreateAccount.dart
+++ lib/ui/CreateAccount.dart
@@ -2,113 +2,125 @@ import 'package:flutter/material.dart';
 import 'themes.dart';
 import 'package:intl/intl.dart';
 
-
 class CreateAccount extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child: ListView(children: <Widget>[
-              const SizedBox(
-                height: 150,
-              ),
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    TextButton(onPressed: (){},
-                        child: const Text('Back to sign in')),]
-              ),
-              CreateAccountForm(key: super.key),
-              const SizedBox(width: 20),
-              Padding(
-                  padding: const EdgeInsets.fromLTRB(100.0,25,100,30),
-                  child:
-                  Column(
+      body: Center(
+          child: Column(children: [
+        Expanded(
+
+            child: NotificationListener<OverscrollIndicatorNotification>(
+              onNotification: (overscroll) {
+                overscroll.disallowIndicator();
+                return true;
+              },
+              child: ListView(children: <Widget>[
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      ElevatedButton(
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: const [
-                              Text(
-                                'Create account',
-                              ),
-                              Icon(Icons.keyboard_arrow_right,
-                                  size: 24.0),]
-                        ),
-                        onPressed: () {
-                          //runApp(CreateAccountPage(key: super.key));
-                        },
-                      ),
                       TextButton(
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: const [
-                              Text(
-                                'Continue as Guest ',
-                              ),
-                              Icon(Icons.person,
-                                  size: 24.0),]
+                          onPressed: () {}, child: const Text('Back to sign in')),
+                    ]),
+                CreateAccountForm(key: super.key),
+                const SizedBox(width: 20),
+                Padding(
+                    padding: const EdgeInsets.fromLTRB(100.0, 25, 100, 30),
+                    child: Column(
+                      children: [
+                        ElevatedButton(
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const [
+                                Text(
+                                  'Create account',
+                                ),
+                                Icon(Icons.keyboard_arrow_right, size: 24.0),
+                              ]),
+                          onPressed: () {
+                            //runApp(CreateAccountPage(key: super.key));
+                          },
                         ),
-                        onPressed: () {
-                          //runApp(CreateAccountPage(key: super.key));
-                        },
-                      ),
-                    ],
-                  )),
-              Image.asset('assets/citysky.png')
-            ])));
+                        TextButton(
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const [
+                                Text(
+                                  'Continue as Guest ',
+                                ),
+                                Icon(Icons.person, size: 24.0),
+                              ]),
+                          onPressed: () {
+                            //runApp(CreateAccountPage(key: super.key));
+                          },
+                        ),
+
+                      ],
+                    )),
+                SizedBox(height: 112,),
+                Align(
+                  alignment: Alignment.center,
+                  child: Image.asset('assets/citysky.png'),
+                )
+              ])),
+            ),
+
+      ])),
+    );
   }
 }
 
-class CreateAccountForm extends StatefulWidget{
+class CreateAccountForm extends StatefulWidget {
   const CreateAccountForm({super.key});
 
   @override
-  State<CreateAccountForm> createState() =>
-      _CreateAccountFormWidgetState();
+  State<CreateAccountForm> createState() => _CreateAccountFormWidgetState();
 }
 
-class _CreateAccountFormWidgetState extends State<CreateAccountForm>{
-
+class _CreateAccountFormWidgetState extends State<CreateAccountForm> {
   var passwordVis = true;
   final TextEditingController _dateInput = TextEditingController();
   DateTime birthday = DateTime.now();
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     //
   }
 
   @override
-  Widget build(BuildContext context){
-
-    return
-      Padding(
+  Widget build(BuildContext context) {
+    return Padding(
       padding: const EdgeInsets.all(10),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Row(
             children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                  icon: Icon(Icons.person),
-                  labelText: 'First Name',
-                ),),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Last Name',
-                ),),
+              Expanded(
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    icon: Icon(Icons.person),
+                    labelText: 'First Name',
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Last Name',
+                  ),
+                ),
+              )
             ],
           ),
           TextField(
             controller: _dateInput,
             //editing controller of this TextField
             decoration: const InputDecoration(
-              labelText:
-              'Enter Birthday',
+              icon: Icon(Icons.calendar_month_outlined),
+              labelText: 'Enter Birthday',
             ),
             readOnly: true,
             onTap: () async {
@@ -118,17 +130,18 @@ class _CreateAccountFormWidgetState extends State<CreateAccountForm>{
                   firstDate: DateTime(1900),
                   lastDate: DateTime.now());
 
-
               if (pickedDate != null) {
-                birthday = pickedDate;//pickedDate output format => 2021-03-10 00:00:00.000
+                birthday =
+                    pickedDate; //pickedDate output format => 2021-03-10 00:00:00.000
                 String formattedDate =
-                DateFormat('MM/dd/yyyy')
-                    .format(pickedDate);
-                print(formattedDate); //formatted date output using intl package =>  2021-03-16
+                    DateFormat('MM/dd/yyyy').format(pickedDate);
+                print(
+                    formattedDate); //formatted date output using intl package =>  2021-03-16
                 //you can implement different kind of Date Format here according to your requirement
 
                 setState(() {
-                  _dateInput.text = formattedDate; //set output date to TextField value.
+                  _dateInput.text =
+                      formattedDate; //set output date to TextField value.
                 });
               } else {
                 print('Date is not selected');
@@ -139,7 +152,8 @@ class _CreateAccountFormWidgetState extends State<CreateAccountForm>{
             decoration: const InputDecoration(
               icon: Icon(Icons.email_sharp),
               labelText: 'E-mail',
-            ),),
+            ),
+          ),
           TextFormField(
             obscureText: passwordVis,
             decoration: InputDecoration(
@@ -149,10 +163,12 @@ class _CreateAccountFormWidgetState extends State<CreateAccountForm>{
                     icon: Icon(
                         passwordVis ? Icons.visibility : Icons.visibility_off),
                     onPressed: () {
-                      setState(() { passwordVis = !passwordVis; },
+                      setState(
+                        () {
+                          passwordVis = !passwordVis;
+                        },
                       );
-                    })
-            ),
+                    })),
           ),
           TextFormField(
             obscureText: passwordVis,
@@ -163,13 +179,15 @@ class _CreateAccountFormWidgetState extends State<CreateAccountForm>{
                     icon: Icon(
                         passwordVis ? Icons.visibility : Icons.visibility_off),
                     onPressed: () {
-                      setState(() { passwordVis = !passwordVis; },
+                      setState(
+                        () {
+                          passwordVis = !passwordVis;
+                        },
                       );
-                    })
-            ),
+                    })),
           ),
         ],
-      ) ,
+      ),
     );
   }
 
